1fc44eb4702870cbb14f1c1a4550d013f3bea61f
Denis Morozov <103926466+denfreeze@users.noreply.github.com>
2024-11-04 10:18:26 +0300

    Version from 04 Nov

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util util = new Util();
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        util.getConnection();
+
+        userDao.createUsersTable();
+        userDao.saveUser("Denis", "Morozov", (byte)30);
+        userDao.saveUser("Ilon", "Mask", (byte)53);
+        userDao.saveUser("Lev", "Tolstoy", (byte)99);
+        userDao.saveUser("Fyodor", "Dostojevsiy", (byte)99);
+
+        System.out.println(userDao.getAllUsers());
+
+        userDao.cleanUsersTable();
+
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/my_schema";
+    private static final String user = "root2";
+    private static final String password = "root";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
